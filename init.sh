1e686f8c666e600ca1f1a0579fada2f7133a32a7
Kaiying <kaiying1991@hotmail.com>
2024-03-23 22:57:58 +0800

    added gedit new window shortcut

--- init.sh
+++ init.sh
@@ -13,6 +13,7 @@ xfconf-query -c xfce4-keyboard-shortcuts -n -t 'string' -p '/commands/custom/<Su
 xfconf-query -c xfce4-keyboard-shortcuts -n -t 'string' -p '/commands/custom/<Super>l' -s 'xflock4'
 xfconf-query -c xfce4-keyboard-shortcuts -n -t 'string' -p '/commands/custom/<Super>r' -s 'xfrun4'
 xfconf-query -c xfce4-keyboard-shortcuts -n -t 'string' -p '/commands/custom/<Ctrl>Escape' -s 'xfce4-popup-zorinmenulite'
+xfconf-query -c xfce4-keyboard-shortcuts -n -t 'string' -p '/commands/custom/<Super>n' -s 'gedit --new-window'
 echo "Setting keyboard shortcuts completed"
 
 # ##################################################
