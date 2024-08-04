316a6b916a1ae1ddc9d21b0d443b266589828292
kaiying <kaiying1991@hotmail.com>
2024-08-05 00:01:04 +0800

    update command

--- server.sh
+++ server.sh
@@ -13,17 +13,18 @@ sudo apt install -y net-tools curl
 curl -o webmin.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
 chmod +x webmin.sh
 sudo ./webmin.sh -f
-sudo apt-get install -y webmin --install-recommends
+sudo apt install -y webmin --install-recommends
 
 # ##################################################
 # Install docker
 # ##################################################
-sudo apt-get install -y ca-certificates
+sudo apt install -y ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # ##################################################
 # Install portainer
