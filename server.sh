f6d393d7db48a08ee319cbc8c375c7678d36f818
kaiying <kaiying1991@hotmail.com>
2024-08-05 19:36:25 +0800

    update apt to apt-get

--- server.sh
+++ server.sh
@@ -1,8 +1,8 @@
 # ##################################################
 # Install common utils
 # ##################################################
-sudo apt update
-sudo apt install -y net-tools curl openssh-server
+sudo apt-get update
+sudo apt-get install -y net-tools curl openssh-server
 
 # ##################################################
 # Install webmin
@@ -13,17 +13,17 @@ sudo apt install -y net-tools curl openssh-server
 curl -o webmin.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
 chmod +x webmin.sh
 sudo ./webmin.sh -f
-sudo apt install -y webmin --install-recommends
+sudo apt-get install -y webmin --install-recommends
 
 # ##################################################
 # Install docker
 # ##################################################
-sudo apt install -y ca-certificates
+sudo apt-get install -y ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # ##################################################
 # Install portainer
@@ -35,8 +35,8 @@ sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -
 # ##################################################
 # APT cleanup
 # ##################################################
-sudo apt -y upgrade
-sudo apt -y autoremove
-sudo apt -y autoclean
-sudo apt -y autopurge
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
+sudo apt-get -y autopurge
 sudo apt-get remove --purge -y `dpkg -l | grep '^rc' | awk '{print $2}'`
