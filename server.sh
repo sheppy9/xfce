818eb82fd8a23f1e527a4b38542dbb1bc63f8f86
kaiying <kaiying1991@hotmail.com>
2024-08-05 19:31:45 +0800

    adjust docker script

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 # Install common utils
 # ##################################################
 sudo apt update
-sudo apt install -y net-tools curl
+sudo apt install -y net-tools curl openssh-server
 
 # ##################################################
 # Install webmin
@@ -23,8 +23,7 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # ##################################################
 # Install portainer
