262c44040fb50b58cec6d57556fdc095f8216518
kaiying <kaiying1991@hotmail.com>
2024-08-05 19:48:39 +0800

    Added wait for lock function

--- server.sh
+++ server.sh
@@ -1,7 +1,17 @@
+# Function to wait for the lock to be released
+wait_for_lock() {
+	while sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1; do
+		echo "Waiting for other apt-get process to finish..."
+		sleep 5
+	done
+}
+
 # ##################################################
 # Install common utils
 # ##################################################
+wait_for_lock
 sudo apt-get update
+wait_for_lock
 sudo apt-get install -y net-tools curl openssh-server
 
 # ##################################################
@@ -13,18 +23,22 @@ sudo apt-get install -y net-tools curl openssh-server
 curl -o webmin.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
 chmod +x webmin.sh
 sudo ./webmin.sh -f
+wait_for_lock
 sudo apt-get install -y webmin --install-recommends
 
 # ##################################################
 # Install docker
 # ##################################################
+wait_for_lock
 sudo apt-get install -y ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sleep 5
-sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+wait_for_lock
+sudo apt-get update
+wait_for_lock
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # ##################################################
 # Install portainer
