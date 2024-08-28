f91929bbbbeaebd1dfeb2c7188ef13b14a2f13b2
kaiying <kaiying.lai@bitopiatech.com>
2024-08-28 15:17:24 +0800

    update xfce4 panel configuration

--- workstation.sh
+++ workstation.sh
@@ -76,9 +76,11 @@ xfconf-query -c xfce4-desktop -t 'bool' -np '/desktop-icon/file-icons/show-home'
 xfconf-query -c xfce4-desktop -t 'bool' -np '/desktop-icon/file-icons/show-removable' -s 'false'
 
 # Panels settings
-xfconf-query -c xfce4-panel -t 'int' -np '/panels/panel-1/size' -s '40'
-xfconf-query -c xfce4-panel -t 'string' -np '/panels/panel-1/position' -s 'p=0;x=0;y=2560'
-xfconf-query -c xfce4-panel -t 'double' -np '/panels/panel-1/length' -s '100'
+# xfconf-query -c xfce4-panel -t 'int' -np '/panels/panel-1/size' -s '40'
+# xfconf-query -c xfce4-panel -t 'string' -np '/panels/panel-1/position' -s 'p=0;x=0;y=2560'
+# xfconf-query -c xfce4-panel -t 'double' -np '/panels/panel-1/length' -s '100'
+curl -o ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml https://raw.githubusercontent.com/kaiying1991/xfce/master/config/xfce4-panel.xml
+xfce4-panel -r
 
 # Power Manager
 xfconf-query -c xfc4-power-manager -t 'int' -np '/xfce4-power-manager/power-button-action' -s '4'
